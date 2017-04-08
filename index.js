b0099108b6aea7a2d66adb4f670469c23ffb8e64
arunkan <ak3xc@virginia.edu>
2017-04-08 11:46:27 -0400

    should send basic messages

--- index.js
+++ index.js
@@ -14,18 +14,11 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 // Index route
-// app.get('/', function (req, res) {
-// 	res.send('Hello world, I am a chat bot')
-// })
+ app.get('/', function (req, res) {
+ 	res.send('Hello world, I am a chat bot')
+ })
 
 // for Facebook verification
-app.get('/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
-})
-
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
