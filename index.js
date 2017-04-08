02e6666873aeefae1d3566f8f935bb49bb22bf6f
arunkan <ak3xc@virginia.edu>
2017-04-08 14:26:40 -0400

    messing with generic

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ function sendGenericMessage(sender, text) {
 		}
 	}
 }
-
+                                                                                  
 if (text.toLowerCase().includes("ohill") || text.toLowerCase().includes("observatory")) {
 		let messageData = {
 	    "attachment": {
@@ -125,29 +125,6 @@ if (text.toLowerCase().includes("ohill") || text.toLowerCase().includes("observa
 		    "payload": {
 				"template_type": "generic",
 			    "elements": [{
-				    "title": "Observatory Hill Dining Room",
-				    "subtitle": "Observatory Hill is located on the southwestern corner of Alderman and McCormick Roads near the first-year residences. More commonly known as O'Hill, this dining room has a large seating area with two levels, and eight stations to choose from! ",
-				    "image_url": "http://virginia.campusdish.com/-/media/Images/Aramark/Higher-Education/Eastern/Virginia/Location/ObservatoryHill-Dining-225x150.jpg?h=150&la=en&w=222&hash=59AFD7D6FE0377A53E164559A695BD8388577DFB",
-				    "buttons": [{
-				    	"type": "web_url",
-				    	"url": "https://www.fm.virginia.edu/fpc/featuredprojects/OHillDining/Photos/Renderings/OhillDiningEntry.jpg",
-				    	"title": "See Menu"
-				    }, {
-						"type": "postback",
-						"title": "Hours of Operation",
-						"payload": "Ohill_Hours",
-					}]
-				}]
-			}
-		}
-	}
-}
-
-if (text.toLowerCase().includes("runk")) {
-		let messageData = {
-	    "attachment": {
-		    "type": "template",
-		    "payload": {
 				    "title": "Runk Dining Room",
 				    "subtitle": "Runk Dining is open to all, but mainly serves the Hereford College and Gooch/Dillard residents. It's convenient to the Student Activities Building, the Piedmont Faculty Housing area, and the McCormick Observatory.",
 				    "image_url": "http://www.globalsustainability.virginia.edu/wp-content/uploads/2014/07/1782519_10204400187669979_8875502862353311649_o-1024x768.jpg",
@@ -165,7 +142,31 @@ if (text.toLowerCase().includes("runk")) {
 		}
 	}
 }
+
+if (text.toLowerCase().includes("runk")) {
+		let messageData = {
+	    "attachment": {
+		    "type": "template",
+		    "payload": {
+				"template_type": "generic",
+			    "elements": [{
+					"title": "Newcomb Dining Hall",
+				    "subtitle": "This 35,000-square-foot renovated facility opened in January 2013 in Newcomb Hall. There are eight prepared-to-order stations, including Mongolian Grill, Deli, Vegan Station, Produce & Salad Bar, and much more! UVa students have embraced the 'new' Newcomb and laud its menu choices as well as its comfortable and fun dining atmosphere.",
+				    "image_url": "https://news.virginia.edu/sites/default/files/article_image/NewcombDining011413_01.jpeg",
+				    "buttons": [{
+					    "type": "web_url",
+					    "url": "http://virginia.campusdish.com/Commerce/Catalog/Menus.aspx?LocationId=704",
+					    "title": "See Menu"
+				    }, {
+				    	"type": "postback",
+				    	"title": "Hours of Operation",
+				    	"payload": "Newc_Hours",
+				    }]
+				}]
+			}
+		}
 	}
+}
     request({
 	    url: 'https://graph.facebook.com/v2.6/me/messages',
 	    qs: {access_token:token},
